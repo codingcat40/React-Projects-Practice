f8cae76eaf5f755d70dce120b762261c87a9824c
Naman Matoliya <namansharma50962@gmail.com>
2025-03-28 20:06:05 +0530

    Add the title Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import styled from 'styled-components'
+import StartGame from './components/StartGame'
+// const Button = styled.button`
+// background-color:  black;
+// color: white;
+// padding: 10px;
 
-const Button = styled.button``
+// `
 
 function App() {
 
   return (
-   <Button>
-      Styled Button
-   </Button>
+   <>
+      <StartGame />
+   </>
   )
 }
 
