608c29f5cd4566227433a21b1648c6a1bfd5a130
Naman Matoliya <namansharma50962@gmail.com>
2025-03-28 18:27:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Button = styled.button``
+
+function App() {
+
+  return (
+   <Button>
+      Styled Button
+   </Button>
+  )
+}
+
+export default App
